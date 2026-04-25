2834b0246c399f197d77b2d117b9110c8f53b07a
sarah <saaramohamez@gmail.com>
2026-04-25 07:25:32 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMenu(){
+  document.getElementById("menu").classList.toggle("active");
+}
+
+// animation
+const cards = document.querySelectorAll(".card");
+
+window.addEventListener("scroll", ()=>{
+  cards.forEach(card=>{
+    const top = card.getBoundingClientRect().top;
+
+    if(top < window.innerHeight - 100){
+      card.classList.add("show");
+    }
+  });
+});
